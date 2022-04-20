71ad19b28c4d2a32c313b873e90bd77f27ba0788
Jamehzz <probableeme@gmail.com>
2022-04-19 22:39:41 -0400

    added boostrap and components to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer/Footer";
+import './components/Section.css';
 
-const App = () => <PortfolioContainer />;
+function App() {
+    return (
+      <div id="app" className="row justify-content-center">
+        <Navbar />
+        
+        <div className="col col-10 p-4 m-1">
+          <About />
+        </div>
+        
+        <div className="col col-10 p-4 m-1">
+          <Projects />
+        </div>
+        
+        <div className="col col-10 p-4 m-1">
+          <Contact />
+        </div>
+        
+        <div className="col col-10 p-4 m-1">
+          <Footer/>
+        </div>
+      </div>
+    );
+  }
 
 export default App;
