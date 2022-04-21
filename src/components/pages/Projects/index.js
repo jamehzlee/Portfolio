185c8cb89cf16c55a7c66da2b9cefa7d5948b9f7
Jamehzz <probableeme@gmail.com>
2022-04-21 17:20:15 -0400

    added Projects page

---
+++ src/components/pages/Projects/index.js
@@ -0,0 +1,42 @@
+// import pokemon from '../../assets/images/pokemon.png';
+// import rpg from '../../assets/images/rpg.png';
+// import weather from '../../assets/images/weather.png';
+// import blog from '../../assets/images/blog.png';
+// import './Projects.css';
+
+function Projects() {
+  return (
+    <div className="container">
+      <h2>Projects</h2>
+      {/* <div className="section">
+        <div className='row justify-content-center p-1'>
+          <div className='col p-0 m-4'>
+            <a href='https://jamehzlee.github.io/Pokemon-Data-Finder/'>
+              <img src={pokemon} alt='Pokemon' className='img-fluid h-100'></img>
+            </a>
+          </div>
+
+          <div className='col p-0 m-4'>
+            <a href='https://bp-rpg-character-database.herokuapp.com/'>
+              <img src={rpg} alt='RPG' className='img-fluid h-100'></img>
+            </a>
+          </div>
+   
+          <div className='col p-0 m-4'>
+            <a href='https://jamehzlee.github.io/Weather-Dashboard/'>
+              <img src={weather} alt='Weather' className='img-fluid h-100'></img>
+            </a>
+          </div>
+
+          <div className='col p-0 m-4'>
+            <a href='https://tech-blog-jlee.herokuapp.com/'>
+              <img src={blog} alt='Tech Blog' className='img-fluid h-100'></img>
+            </a>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default Projects;
