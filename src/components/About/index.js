3cb1da468a8e6b66b8980e2d4da452c0d03943d0
Jamehzz <probableeme@gmail.com>
2022-04-21 17:17:10 -0400

    added new About component

---
+++ src/components/About/index.js
@@ -0,0 +1,23 @@
+import './index.css';
+import profile from '../../assets/images/profile.png'
+
+const About = () => {
+    return (
+        <div className="row about-row justify-content-center ">
+            <h2>About Me</h2>
+            <div className='row justify-content-center'>
+                <div className='col-md-3 '>
+                <img src={profile} alt ='Profile' className='img-fluid p-3'></img>
+                </div>
+                <div className='col-md-6 align-self-center'>
+                <p>
+                I am a Full Stack Developer who learned the MERN stack at the Rutgers University Coding Bootcamp.
+                In just a few months I have already learned so much and am excited for what I will learn for years to come.
+                </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
