e9d1d7eb72ed8968edc0513e4ba470a7d93c06e2
Jamehzz <probableeme@gmail.com>
2022-04-21 17:19:48 -0400

    added Contact page

---
+++ src/components/pages/Contact/index.js
@@ -0,0 +1,13 @@
+// import './Section.css';
+
+function Contact() {
+  return (
+    <section id='contact' className="">
+      {/* <h2>Contact Me</h2>
+      <a href='mailto:this.jameslee96@gmail.com'>this.jameslee96@gmail.com</a>
+      <p>Phone #: 201-364-3636</p> */}
+    </section>
+  );
+}
+
+export default Contact;
