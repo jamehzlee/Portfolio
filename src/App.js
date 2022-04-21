0ede10ba159c00cb9c2ae87ab5b7addc70814d78
Jamehzz <probableeme@gmail.com>
2022-04-21 17:28:57 -0400

    added App.js with Routes implementation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer/Footer";
-import './components/Section.css';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import About from './components/About';
+import { Projects, Contact, Resume } from './components/pages';
 
 function App() {
-    return (
-      <div id="app" className="row justify-content-center">
-        <Navbar />
-        
-        <div className="col col-10 p-4 m-1">
-          <About />
-        </div>
-        
-        <div className="col col-10 p-4 m-1">
-          <Projects />
-        </div>
-        
-        <div className="col col-10 p-4 m-1">
-          <Contact />
-        </div>
-        
-        <div className="col col-10 p-4 m-1">
-          <Footer/>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <>
+      <Routes>
+          <Route path='/' element={<Layout />} >
+            <Route index element={<About />} />
+            <Route path='projects' element={<Projects />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='resume' element={<Resume />} />
+          </Route>
+      </Routes>
+    </>
+  );
+}
 
 export default App;
