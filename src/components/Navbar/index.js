c291bc98746c277fa824a671eac0672e5bff4bba
Jamehzz <probableeme@gmail.com>
2022-04-23 10:28:51 -0400

    fixed navbar styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,18 +6,25 @@ import './index.css';
 const Navi = () => {
 
   return (
-  <Navbar expand="lg" variant='dark' className=''>
-  <Navbar.Brand className='name'>James Lee</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse>
-    <Nav >
-      <Nav.Link as={NavLink} to='/'>About</Nav.Link>
-      <Nav.Link as={NavLink} to="/projects">Projects</Nav.Link>
-      <Nav.Link as={NavLink} to="#pricing">Resume</Nav.Link>
-      <Nav.Link as={NavLink} to="#pricing">Contact Me</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+  <Navbar collapseOnSelect='true' expand="lg" variant='dark'>
+
+    <Navbar.Brand className='name'>James Lee</Navbar.Brand>
+    
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse>
+
+      <Nav>
+        <Nav.Link as={NavLink} to='/'>About</Nav.Link>
+
+        <Nav.Link as={NavLink} to="/projects">Projects</Nav.Link>
+
+        <Nav.Link as={NavLink} to="/contact">Contact Me</Nav.Link>
+
+        <Nav.Link as={NavLink} to="/resume">Resume</Nav.Link>
+      </Nav>
+
+    </Navbar.Collapse>
+  </Navbar>
   );
 }
 
