121e93596777f87e525fd2d74f71c4be8d0f99a3
Jamehzz <probableeme@gmail.com>
2022-05-18 15:15:26 -0400

    added back emailjs api key and configured contact form submit

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,49 +1,60 @@
 import './index.css';
-import { Form, Button } from 'react-bootstrap'
-import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
   const [valid, setValid] = useState(false);
-
-  // const handleChange = (e) => {
-  //   setValid({email: e.target.value});
-  // };
+  const refForm = useRef();
   
+  const sendEmail = (e) => {
+    emailjs
+      .sendForm('service_b32cnvg', 'template_kz0n9wn', refForm.current, '0DQLz8bRtTvmQgTKL')
+      .then(function(response) {
+         console.log('SUCCESS!', response.status, response.text);
+      }, function(error) {
+         console.log('FAILED...', error);
+      });
+  }
+
   const handleSubmit = (e) => {
     const form = e.currentTarget;
     if (form.checkValidity() === false) {
       e.preventDefault();
       e.stopPropagation();
     }
-
+    
     setValid(true);
+    console.log(form);
+    sendEmail(form)
   };
+
 
   return (
     <div className="row">
-      <Form noValidate validated={valid} onSubmit={handleSubmit} className='col-4'>
-        <Form.Group className="mb-3" controlId="">
-          <Form.Control required type="email" placeholder="Email" />
+      <Form ref={refForm} noValidate validated={valid} onSubmit={handleSubmit} className='col-4'>
+        <Form.Group className="mb-3">
+          <Form.Control required type="email" name='user_email' placeholder="Email" />
           <Form.Control.Feedback type="invalid">
             Please provide a email.
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className="mb-4" controlId="">
-          <Form.Control required type='text' placeholder="Subject" />
+        <Form.Group className="mb-4">
+          <Form.Control required type='text' name='subject' placeholder="Subject" />
           <Form.Control.Feedback type="invalid">
             Please add a subject.
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className="mb-5" controlId="">
-          <Form.Control required as='textarea' rows={5} placeholder="Message" />
+        <Form.Group className="mb-5">
+          <Form.Control required as='textarea' rows={5} name='message' placeholder="Message" />
           <Form.Control.Feedback type="invalid">
             Please do not leave the message blank.
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Button variant="light" type="submit">
+        <Button variant="light" type="submit" value="Send">
           Submit
         </Button>
       </Form>
