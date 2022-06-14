05d45cb666b9110cf8469509d7bfb165a4861507
Jamehzz <probableeme@gmail.com>
2022-06-14 13:57:18 -0400

    created card component for projects page

---
+++ src/components/Card/index.js
@@ -0,0 +1,23 @@
+import "./index.css";
+import React from "react";
+import { CardActionArea, CardMedia, Grid, Grow } from "@mui/material";
+import { Card as ProjectCard } from "@mui/material";
+
+export default class Card extends React.Component {
+  render() {
+ 
+    return (
+      <Grid item>
+        <Grow in={true} timeout={1000}>
+          <ProjectCard>
+            <CardActionArea sx={{ maxWidth: 500, maxHeight: 250 }}>
+              <a href={this.props.projectLinks} target="_blank" rel="noreferrer">
+                <CardMedia component="img" src={this.props.src} alt="freelancer" />
+              </a>
+            </CardActionArea>
+          </ProjectCard>
+        </Grow>
+      </Grid>
+    );
+  }
+}
