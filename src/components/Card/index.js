dcbbfef6a066f150a70af91e5d2bc21eff9a6444
Jamehzz <probableeme@gmail.com>
2022-06-24 19:07:35 -0400

    updated to use props and mui

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,23 +1,47 @@
+import React, {useState} from "react";
+import { Card as ProjectCard } from "@mui/material/";
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from "@mui/material/Grid";
+import Grow from "@mui/material/Grow";
+import Typography from "@mui/material/Typography";
 import "./index.css";
-import React from "react";
-import { CardActionArea, CardMedia, Grid, Grow } from "@mui/material";
-import { Card as ProjectCard } from "@mui/material";
+import styled from 'styled-components';
 
-export default class Card extends React.Component {
-  render() {
- 
-    return (
-      <Grid item>
-        <Grow in={true} timeout={1000}>
-          <ProjectCard>
-            <CardActionArea sx={{ maxWidth: 500, maxHeight: 250 }}>
-              <a href={this.props.projectLinks} target="_blank" rel="noreferrer">
-                <CardMedia component="img" src={this.props.src} alt={this.props.name} />
-              </a>
-            </CardActionArea>
-          </ProjectCard>
-        </Grow>
-      </Grid>
-    );
+const CustomItem = styled(Grid)`
+  &:hover {
+    transform: scale(1.025);
+    opacity: 50%;
   }
+`;
+
+const Card = (props) => {
+  // const [hover, setHover] = useState(false);
+  // const handleHover = () => {
+  //   hover === true ? setHover(false) : setHover(true);
+  // }
+
+  return (
+    <CustomItem item>
+      <Grow in={true} timeout={1000}>
+
+        <ProjectCard raised={true} >
+          <CardActionArea sx={{ maxWidth: 500, maxHeight: 250 }}>
+            <a href={props.projectLinks} target="_blank" rel="noreferrer">
+              <CardMedia component="img" src={props.src} alt={props.name} />
+              <CardContent>
+                <Typography>
+                {props.name}
+                </Typography>
+              </CardContent>
+            </a>
+          </CardActionArea>
+        </ProjectCard>
+
+      </Grow>
+    </CustomItem>
+  );
 }
+
+export default Card;
