44921be1415ea76725899bec109ae1870b7155be
Jamehzz <probableeme@gmail.com>
2022-06-28 17:15:06 -0400

    added mui container to focus content in center

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,13 +2,18 @@ import './index.css';
 import NavBar from '../Navbar';
 import Footer from '../Footer';
 import { Outlet } from 'react-router-dom';
+import { Container } from '@mui/system';
 
 const Layout = () => {
     return (
         <div className='row body-row'>
             <NavBar />
-            
-            <Outlet />
+
+            <Container>
+
+                <Outlet />
+                
+            </Container>
 
             <Footer />
         </div>
